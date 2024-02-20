420bdc9cd79aad027ad4d1343e07192558a95424
AlvinDwiJayadi <alvinxdj444@gmail.com>
2024-02-20 13:27:47 +0700

    first commit

---
+++ src/main/java/com/mycompany/mavenproject1/NewMain.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alvin
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s=new Scanner(System.in);
+        System.out.print("Masukkan Panjang : ");
+        int panjang=s.nextInt();
+        System.out.print("Masukkan Lebar : ");
+        int lebar=s.nextInt();
+        int luas=panjang*lebar;
+        System.err.println("Luas persegi panjang : "+luas);
+    }
+    
+}
